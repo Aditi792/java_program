7f55a7ec3555f083eb37b878a3e54d96691e1bed
Aditi <aditimondalgc@gmail.com>
2024-07-08 12:49:16 +0530

    array easy problem

---
+++ java_DSA/array/Longest_SubarraySize_target_Positive_Negetive.java
@@ -0,0 +1,54 @@
+package java_DSA.array;
+
+import java.util.HashMap;
+
+/*
+ * Given an array arr containing n integers and an integer k. 
+ * Your task is to find the length of the longest Sub-Array with the sum of the elements equal to the given value k.
+
+
+Input :
+arr[] = {10, 5, 2, 7, 1, 9}, k = 15
+Output : 4
+Explanation:
+The sub-array is {5, 2, 7, 1}.
+
+This is the better approach for positive and negetive numbers in array.
+ */
+
+public class Longest_SubarraySize_target_Positive_Negetive {
+
+    public static int longSubArraySize(int[] arr , int K){
+
+        HashMap <Integer ,Integer> subArr = new HashMap<>();
+
+        int n = arr.length;
+        int maxlen=0;
+        int sum = 0;
+        int rem = 0;
+
+        for(int  i=0 ; i<n ; i++){
+            sum += arr[i];
+            if(sum == K){
+                maxlen = Math.max(maxlen, i+1 );
+            }
+            rem = sum-K;
+            
+            if (subArr.containsKey(rem)){
+                int len = subArr.get(rem);
+                maxlen = Math.max(maxlen, len) ;
+            }
+
+            if( !subArr.containsKey(sum)){
+                subArr.put(sum, i);
+            }  
+        }
+        return maxlen;
+
+    }
+
+    public static void main(String[] args) {
+        int arr[] = {10, 5, 2, 7, 1, 9};
+        System.out.print("Longest Subarray size is : " + longSubArraySize(arr, 10));
+    }
+}
---
+++ java_DSA/array/two_sum.java
@@ -0,0 +1,75 @@
+package java_DSA.array;
+
+import java.util.HashMap;
+
+public class two_sum {
+
+    public static boolean TwoSum(int[] arr , int target){
+
+        int n=arr.length;
+
+        //for brute force solution with TC = O(n^2) SC = O(1)
+        /*
+        for(int i=0 ; i < n ; i ++){
+            for(int j = i+1 ; j<n ; j++){
+                if(arr[i] + arr[j] == target){
+                    return true;
+                }
+            }
+        }
+        return false;
+
+
+        
+    /*
+        //better option with TC = O(n) and SC= O(n)
+
+        int need = 0;
+        HashMap <Integer,Integer> check = new HashMap<>();
+        for(int i =0 ; i<n ; i++){
+            need = target - arr[i];
+            if( !check.containsKey(need)){
+                check.put(arr[i], i);
+            }
+            else{
+                return true;
+            }
+        }
+        return false;
+    }
+    */ 
+
+
+
+        // optimal solution for true false return
+
+        
+        int left = 0;
+        int right = arr.length-1;
+        while(left < right){
+            if(arr[left] + arr[right] > target){
+                right--;
+            }
+            if(arr[left] + arr[right] < target){
+                left++;
+            }
+            if(arr[left] + arr[right] == target){
+                return true;
+            }
+        }
+        return false;
+    }
+
+
+
+
+    public static void main(String[] args) {
+        int nums[] = {2,7,11,15};
+        if(TwoSum(nums, 10)){
+            System.out.println("Integer are present which sum gives the target output.");
+        }
+        else{
+            System.out.println("Integer are not present in the array");
+        }
+    }
+}
