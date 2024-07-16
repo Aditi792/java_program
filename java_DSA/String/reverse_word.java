39808533f366e63581c0e44c56c140288a8dded0
Aditi <aditimondal792@gmail.com>
2024-07-16 21:45:17 +0530

    reverse string with words

--- java_DSA/String/reverse_word.java
+++ java_DSA/String/reverse_word.java
@@ -1,9 +1,30 @@
 package java_DSA.String;
+/*
+ * Given an input string s, reverse the order of the words.
+A word is defined as a sequence of non-space characters. The words in s will be separated by at least one space.
+Return a string of the words in reverse order concatenated by a single space.
+Note that s may contain leading or trailing spaces or multiple spaces between two words. The returned string should only have a single space separating the words. Do not include any extra spaces.
+
+Example 1:
+
+Input: s = "the sky is blue"
+Output: "blue is sky the"
+Example 2:
+
+Input: s = "  hello world  "
+Output: "world hello"
+Explanation: Your reversed string should not contain leading or trailing spaces.
+Example 3:
+
+Input: s = "a good   example"
+Output: "example good a"
+Explanation: You need to reduce multiple spaces between two words to a single space in the reversed string.
+ */
 
 class ReverseWordsInPlace {
 
       //we have to do it in O(1) space complexity
-      
+
     public static String reverseWords(String s) {
         // Convert string to char array for in-place modification
         char[] str = s.toCharArray();
