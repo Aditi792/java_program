b53fb387488282b1e01f544563923f23b27d081e
Aditi <aditimondal792@gmail.com>
2024-07-18 11:35:21 +0530

    longest subarray with sum k

---
+++ java_DSA/String/longest_common_prefix.java
@@ -0,0 +1,17 @@
+package java_DSA.String;
+
+public class longest_common_prefix {
+
+    public static String longestCommonPrefix(String[] strs) {
+        int n = strs.length;
+
+        for(int i = n; i>=0 ; i++){
+            
+        }
+
+        return sb;
+    }
+    public static void main(String[] args) {
+        String[] strs = {"flower","flow","flight"};
+    }
+}
